b9d8ae4180dad8ff7d5e602b592eb43d7434d24d
usuario <usuario@iMac-de-EDINBA173.local>
2019-11-05 18:20:46 -0600

    jorge

---
+++ index.js
@@ -0,0 +1,16 @@
+//menu responsive
+
+let muestraMenu = false;
+$('.hamburger').on('click', function(){
+  if(muestraMenu === true){
+    //$('div').css('right', '-300px');
+    TweenLite.to('.mobile-menu ul', 0.3, 
+                 {right: '-50%'})
+    muestraMenu = false;
+  } else {
+    //$('div').css('right', '0')
+    TweenLite.to('.mobile-menu ul', 0.3,
+                {right: '0'})
+    muestraMenu = true;
+  }
+})
